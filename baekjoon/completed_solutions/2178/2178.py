08203812b921a4cd6dcb2db48066bb3e44da09b7
KimJayhyun <dnlwjem000@gmail.com>
2024-09-20 02:02:34 +0900

    temp

--- baekjoon/completed_solutions/2178/2178.py
+++ baekjoon/completed_solutions/2178/2178.py
@@ -14,43 +14,59 @@ def solution():
     max_row, max_col, data = get_inputs()
 
     dfs = [(0, 0)]
+    visited = []
+
     count = 0
 
     while True:
-        current_position = dfs[-1]
-        print(current_position)
+        current_position = dfs.pop()
+        visited.append(current_position)
+
         if current_position == (max_row - 1, max_col - 1):
             break
         current_row, current_col = current_position
 
         count += 1
 
-        # 아래로 이동
-        if current_row + 1 < max_row:
-            if data[current_row + 1][current_col] == "1":
-                print("아래")
-                dfs.append((current_row + 1, current_col))
-                continue
-
         # 오른쪽으로 이동
         if current_col + 1 < max_col:
-            if data[current_row][current_col + 1] == "1":
+            if (
+                data[current_row][current_col + 1] == "1"
+                and (current_row, current_col + 1) not in visited
+            ):
                 dfs.append((current_row, current_col + 1))
+                continue
+
+        # 아래로 이동
+        if current_row + 1 < max_row:
+            if (
+                data[current_row + 1][current_col] == "1"
+                and (current_row + 1, current_col) not in visited
+            ):
+                dfs.append((current_row + 1, current_col))
                 continue
 
         # 위로 이동
         if current_row - 1 >= 0:
-            if data[current_row - 1][current_col] == "1":
+            if (
+                data[current_row - 1][current_col] == "1"
+                and (current_row - 1, current_col) not in visited
+            ):
                 dfs.append((current_row - 1, current_col))
                 continue
 
         # 왼쪽으로 이동
         if current_col - 1 >= 0:
-            if data[current_row][current_col - 1] == "1":
+            if (
+                data[current_row][current_col - 1] == "1"
+                and (current_row, current_col - 1) not in visited
+            ):
                 dfs.append((current_row, current_col - 1))
                 continue
 
-        dfs.pop()
+        visited.pop()
+
+    return len(visited)
 
 
 def main():
