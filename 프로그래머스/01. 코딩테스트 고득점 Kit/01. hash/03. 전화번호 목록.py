009907558f826bad15a5b96e952a42b4b2e40e76
KimJayhyun <dnlwjem000@gmail.com>
2026-02-26 14:44:16 +0900

    프로그래머스: 해쉬 - solved 03

--- 프로그래머스/01. 코딩테스트 고득점 Kit/01. hash/03. 전화번호 목록.py
+++ 프로그래머스/01. 코딩테스트 고득점 Kit/01. hash/03. 전화번호 목록.py
@@ -2,5 +2,19 @@
 
 
 def solution(phone_book):
-    answer = True
-    return answer
+    phone_set = set()
+
+    for phone in phone_book:
+        phone_set.add(phone)
+
+    for phone in phone_set:
+        for i in range(len(phone)):
+            if phone[:i] in phone_set:
+                return False
+
+    return True
+
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123", "456", "789"]))
+print(solution(["12", "123", "1235", "567", "88"]))
