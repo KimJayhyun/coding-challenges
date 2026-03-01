309e5986f1de544d298e4401ba95864b0866545a
KimJayhyun <dnlwjem000@gmail.com>
2026-03-01 16:15:11 +0900

    04. 프로세스 - 더 나은 풀이

--- 프로그래머스/01. 코딩테스트 고득점 Kit/02. stack & queue/04. 프로세스.py
+++ 프로그래머스/01. 코딩테스트 고득점 Kit/02. stack & queue/04. 프로세스.py
@@ -2,39 +2,55 @@
 
 
 def solution(priorities, location):
-    import heapq
     from collections import deque
 
-    # max 값
-    priorities_heap = [-priority for priority in priorities]
-    heapq.heapify(priorities_heap)
-
-    location_list = [i for i in range(len(priorities))]
-
-    priorities_queue = deque(priorities)
-    location_queue = deque(location_list)
-
+    queue = deque(enumerate(priorities))
     result = 0
-    is_max_priority = False
-    max_priority = -1 * heapq.heappop(priorities_heap)
-    while priorities_queue:
-        process_priority = priorities_queue.popleft()
-        process_location = location_queue.popleft()
 
-        is_max_priority = process_priority == max_priority
-        if not is_max_priority:
-            priorities_queue.append(process_priority)
-            location_queue.append(process_location)
+    while queue:
+        current = queue.popleft()
+        if any(current[1] < p[1] for p in queue):
+            queue.append(current)
         else:
-            if priorities_heap:
-                max_priority = -1 * heapq.heappop(priorities_heap)
-
             result += 1
+            if current[0] == location:
+                return result
 
-            if location == process_location:
-                break
 
-    return result
+# def solution(priorities, location):
+#     import heapq
+#     from collections import deque
+
+#     # max 값
+#     priorities_heap = [-priority for priority in priorities]
+#     heapq.heapify(priorities_heap)
+
+#     location_list = [i for i in range(len(priorities))]
+
+#     priorities_queue = deque(priorities)
+#     location_queue = deque(location_list)
+
+#     result = 0
+#     is_max_priority = False
+#     max_priority = -1 * heapq.heappop(priorities_heap)
+#     while priorities_queue:
+#         process_priority = priorities_queue.popleft()
+#         process_location = location_queue.popleft()
+
+#         is_max_priority = process_priority == max_priority
+#         if not is_max_priority:
+#             priorities_queue.append(process_priority)
+#             location_queue.append(process_location)
+#         else:
+#             if priorities_heap:
+#                 max_priority = -1 * heapq.heappop(priorities_heap)
+
+#             result += 1
+
+#             if location == process_location:
+#                 break
+
+#     return result
 
 
 print(solution([2, 1, 3, 2], 2))  # 1
