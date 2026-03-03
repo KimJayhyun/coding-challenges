685c891c18c0def69e4c2dc39249f191c9db81f2
KimJayhyun <dnlwjem000@gmail.com>
2026-03-03 21:00:21 +0900

    01. 더 맵게 - Refactoring

--- 프로그래머스/01. 코딩테스트 고득점 Kit/03. heap/01. 더 맵게.py
+++ 프로그래머스/01. 코딩테스트 고득점 Kit/03. heap/01. 더 맵게.py
@@ -7,23 +7,41 @@ def solution(scoville, K):
     heapq.heapify(scoville)
 
     count = 0
-    while True:
+    while scoville[0] < K:
+        if len(scoville) == 1:
+            return -1
+
+        count += 1
         latest_value = heapq.heappop(scoville)
-
-        if not scoville:
-            if latest_value < K:
-                return -1
-            else:
-                break
-        elif latest_value < K:
-            count += 1
-
-            second_latest_value = heapq.heappop(scoville)
-            heapq.heappush(scoville, latest_value + 2 * second_latest_value)
-        else:
-            break
+        second_latest_value = heapq.heappop(scoville)
+        heapq.heappush(scoville, latest_value + 2 * second_latest_value)
 
     return count
+
+
+# def solution(scoville, K):
+#     import heapq
+
+#     heapq.heapify(scoville)
+
+#     count = 0
+#     while True:
+#         latest_value = heapq.heappop(scoville)
+
+#         if not scoville:
+#             if latest_value < K:
+#                 return -1
+#             else:
+#                 break
+#         elif latest_value < K:
+#             count += 1
+
+#             second_latest_value = heapq.heappop(scoville)
+#             heapq.heappush(scoville, latest_value + 2 * second_latest_value)
+#         else:
+#             break
+
+#     return count
 
 
 print(solution([1, 2, 3, 9, 10, 12], 7))  # 2
