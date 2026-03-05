a5c7f797f57f425f5fa169491703ac1055fde42e
KimJayhyun <dnlwjem000@gmail.com>
2026-03-05 13:13:06 +0900

    01. 최소직사각형

---
+++ 프로그래머스/01. 코딩테스트 고득점 Kit/04. 완전탐색/01. 최소직사각형.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+
+def solution(sizes):
+    max_w = -1
+    max_h = -1
+    for size in sizes:
+        if size[0] >= size[1]:
+            max_w = max(max_w, size[0])
+            max_h = max(max_h, size[1])
+        else:
+            max_w = max(max_w, size[1])
+            max_h = max(max_h, size[0])
+
+    return max_w * max_h
+
+
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))  # 	4000
+print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))  # 120
+print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))  # 133
