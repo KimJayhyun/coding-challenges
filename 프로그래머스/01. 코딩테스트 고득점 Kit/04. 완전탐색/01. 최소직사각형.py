93746435ee5c7d4621053062ae072814a092fa75
KimJayhyun <dnlwjem000@gmail.com>
2026-03-05 13:17:58 +0900

    01. 최소직사각형 - generator 도입

--- 프로그래머스/01. 코딩테스트 고득점 Kit/04. 완전탐색/01. 최소직사각형.py
+++ 프로그래머스/01. 코딩테스트 고득점 Kit/04. 완전탐색/01. 최소직사각형.py
@@ -2,17 +2,24 @@
 
 
 def solution(sizes):
-    max_w = -1
-    max_h = -1
-    for size in sizes:
-        if size[0] >= size[1]:
-            max_w = max(max_w, size[0])
-            max_h = max(max_h, size[1])
-        else:
-            max_w = max(max_w, size[1])
-            max_h = max(max_h, size[0])
+    max_w = max(max(w, h) for w, h in sizes)  # max(generator)
+    max_h = max(min(w, h) for w, h in sizes)
 
     return max_w * max_h
+
+
+# def solution(sizes):
+#     max_w = -1
+#     max_h = -1
+#     for size in sizes:
+#         if size[0] >= size[1]:
+#             max_w = max(max_w, size[0])
+#             max_h = max(max_h, size[1])
+#         else:
+#             max_w = max(max_w, size[1])
+#             max_h = max(max_h, size[0])
+
+#     return max_w * max_h
 
 
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))  # 	4000
